84b5b6bf3447f71e0ad68af5ba8e02f451a02689
Audrius Mecionis <audrius.mecionis@gmail.com>
2019-02-18 16:33:57 +0200

    Updates

---
+++ precache-manifest.e8cf1e5628a0a00dd166131e5486c843.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ab056748a505c1f086d",
+    "url": "/game-of-life/static/css/main.37121740.chunk.css"
+  },
+  {
+    "revision": "4ab056748a505c1f086d",
+    "url": "/game-of-life/static/js/main.4ab05674.chunk.js"
+  },
+  {
+    "revision": "9c4c57eb71e86f7e0102",
+    "url": "/game-of-life/static/js/1.9c4c57eb.chunk.js"
+  },
+  {
+    "revision": "48c5f91bb349b88c84a7",
+    "url": "/game-of-life/static/js/runtime~main.48c5f91b.js"
+  },
+  {
+    "revision": "e26fd2033350e219f4f478632629b23e",
+    "url": "/game-of-life/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/game-of-life/precache-manifest.e8cf1e5628a0a00dd166131e5486c843.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/game-of-life/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
